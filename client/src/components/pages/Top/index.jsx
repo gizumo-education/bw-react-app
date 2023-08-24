d119f412760cb09422b262ab1f27eeb835ca4799
kensansansansan <kensansansansan@gmail.com>
2023-08-24 10:53:52 +0900

    ToDoの一覧表示

--- client/src/components/pages/Top/index.jsx
+++ client/src/components/pages/Top/index.jsx
@@ -1,11 +1,29 @@
+import { useState, useEffect } from 'react' // レンダリングするのを防ぐことができるuseEffect、useState
+import { axios } from '../../../utils/axiosConfig' // API取得インポート
+
 import { Layout } from '../../ui/Layout'
+import { ListItem } from '../../ui/ListItem' // APIで取得した、3つのToDoを表示するためのコンポーネント
 
 import styles from './index.module.css'
 
 export const Top = () => {
+  const [todos, setTodos] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/todo').then(({ data }) => {
+
+      setTodos(data); // setTodosの呼び出しでtodosステートにデータをセット
+    });
+  }, []);
+
   return (
     <Layout>
       <h1 className={styles.heading}>ToDo一覧</h1>
+      <ul className={styles.list}>
+        {todos.map((todo) => {
+          return <ListItem key={todo.id} todo={todo} />;
+        })}
+      </ul>
     </Layout>
-  )
+  );
 }
