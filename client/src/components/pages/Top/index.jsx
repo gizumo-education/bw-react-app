9e3ff317271a675700e1abac0eb5b76459925ff7
kensansansansan <kensansansansan@gmail.com>
2023-08-24 16:00:14 +0900

    ToDoの追加

--- client/src/components/pages/Top/index.jsx
+++ client/src/components/pages/Top/index.jsx
@@ -1,20 +1,62 @@
-import { useState, useEffect } from 'react' // レンダリングするのを防ぐことができるuseEffect、useState
+import { useState, useEffect, useCallback } from 'react' // レンダリングするのを防ぐことができるuseEffect、useState、useCallback
 import { axios } from '../../../utils/axiosConfig' // API取得インポート
 
 import { Layout } from '../../ui/Layout'
 import { ListItem } from '../../ui/ListItem' // APIで取得した、3つのToDoを表示するためのコンポーネント
 
+import { Button } from '../../ui/Button' // ボタンのコンポーネント
+import { Icon } from '../../ui/Icon' // アイコンのコンポーネント
+import { Form } from '../../ui/Form' // フォームのコンポーネント
+
 import styles from './index.module.css'
 
 export const Top = () => {
-  const [todos, setTodos] = useState([])
-
   useEffect(() => {
     axios.get('http://localhost:3000/todo').then(({ data }) => {
-
       setTodos(data); // setTodosの呼び出しでtodosステートにデータをセット
     });
   }, []);
+
+  const [todos, setTodos] = useState([])
+
+  const [inputValues, setInputValues] = useState({
+    title: '',
+    description: '',
+  })
+  const [isAddTaskFormOpen, setIsAddTaskFormOpen] = useState(false)
+
+  const handleAddTaskButtonClick = useCallback(() => {
+    setIsAddTaskFormOpen(true)
+  }, [])
+
+  const handleCancelButtonClick = useCallback(() => {
+    setInputValues({
+      title: '',
+      description: '',
+    });
+    setIsAddTaskFormOpen(false)
+  }, [])
+
+  const handleInputChange = useCallback((event) => {
+    const { name, value } = event.target
+    setInputValues((prev) => ({ ...prev, [name]: value }))
+  }, [])
+
+  const handleCreateTodoSubmit = useCallback(
+    (event) => {
+      event.preventDefault()
+      axios.post('http://localhost:3000/todo', inputValues).then(({ data }) => {
+        console.log(data)
+        setTodos(handleInputChange => [...handleInputChange, data]);
+        setIsAddTaskFormOpen(false);
+        setInputValues({
+          title: '',
+          description: '',
+        });
+      });
+    },
+    [inputValues]
+  )
 
   return (
     <Layout>
@@ -23,6 +65,29 @@ export const Top = () => {
         {todos.map((todo) => {
           return <ListItem key={todo.id} todo={todo} />;
         })}
+        <li>
+          {isAddTaskFormOpen ? (
+            <Form
+              value={inputValues}
+              onCancelClick={handleCancelButtonClick}
+              onChange={handleInputChange}
+              onSubmit={handleCreateTodoSubmit}
+            />
+          ) : (
+            <Button buttonStyle='indigo-blue'
+              onClick={handleAddTaskButtonClick}
+              className={styles['add-task']}
+            >
+              <Icon
+                iconName='plus'
+                color='orange'
+                size='medium'
+                className={styles['plus-icon']}
+              />
+              タスクを追加
+            </Button>
+          )}
+        </li>
       </ul>
     </Layout>
   );
