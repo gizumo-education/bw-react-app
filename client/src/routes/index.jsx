3889786a7c007d76069854ed5c30a55a74b58758
SuyamaAyaka <ay.20150013@gmail.com>
2023-08-29 15:49:04 +0900

    Section20 SPA 練習問題回答

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -1,7 +1,11 @@
 import { createBrowserRouter } from 'react-router-dom'
 
 import { Top } from '../components/pages/Top'
+import { Completed } from '../components/pages/Completed'
 
 export const router = createBrowserRouter([
   { path: '/', element: <Top /> },
+  // ▼練習問題  サイドバーの「完了済み」をクリックすると完了済み一覧ページに遷移する＊＊＊＊＊＊＊＊＊▼
+  { path: '/completed', element: <Completed /> }
+  // ▲＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊▲
 ])
