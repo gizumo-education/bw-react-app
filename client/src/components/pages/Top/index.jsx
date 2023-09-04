1e6223f102e0788a907564729884866d85cedc42
Ibuki8 <ibukixox@gmail.com>
2023-09-04 12:21:02 +0900

    react sec16 Todoの編集

--- client/src/components/pages/Top/index.jsx
+++ client/src/components/pages/Top/index.jsx
@@ -11,6 +11,7 @@ import styles from './index.module.css'
 
 export const Top = () => {
   const [todos, setTodos] = useState([])  //所得したtodoの一覧管理
+  const [editTodoId, setEditTodoId] = useState('') //編集ボタンがクリックされた時に編集するTodoのidを格納
   const [inputValues, setInputValues] = useState({  //追加フォームに入力された値を保持
     title: '',
     description: '',
@@ -18,10 +19,13 @@ export const Top = () => {
   const [isAddTaskFormOpen, setIsAddTaskFormOpen] = useState(false) //追加フォームの表示、非表示を切り替える
 
   const handleAddTaskButtonClick = useCallback(() => {
+    setInputValues({ title: '', description: '' })
+    setEditTodoId('')
     setIsAddTaskFormOpen(true)
   }, []) //タスクを追加をクリックしたときtrueに
 
   const handleCancelButtonClick = useCallback(() => {
+    setEditTodoId('')
     setIsAddTaskFormOpen(false)
   }, []) //追加フォームのキャンセルボタンをクリックしたときfalseに
 
@@ -30,7 +34,7 @@ export const Top = () => {
     setInputValues((prev) => ({ ...prev, [name]: value }))
   }, [])
 
-  const handleCreateTodoSubmit = useCallback(
+  const handleCreateTodoSubmit = useCallback(  //新しいTodoリスト内容を送信
     (event) => {
       event.preventDefault()
       axios.post('http://localhost:3000/todo', inputValues).then(({ data }) => {
@@ -45,6 +49,39 @@ export const Top = () => {
     [inputValues]
   )
 
+  const handleEditedTodoSubmit = useCallback(  //編集 Todoのタイトル、説明を更新
+    (event) => {
+      event.preventDefault()
+      axios
+        .patch(`http://localhost:3000/todo/${editTodoId}`, inputValues)
+        .then(({ data }) => {
+          setTodos((prevTodos) =>
+          prevTodos.map((todo) =>
+            todo.id === editTodoId ? { ...todo, ...inputValues } : todo
+          ))
+          setEditTodoId('')
+          setInputValues({
+          title: '',
+          description: '',
+          })
+        })
+    },
+    [editTodoId, inputValues]
+  )
+
+
+  const handleEditButtonClick = useCallback((id) => {
+    setIsAddTaskFormOpen(false)
+    setEditTodoId(id)
+    const targetTodo = todos.find((todo) => todo.id === id)
+    setInputValues({
+      title: targetTodo.title,
+      description: targetTodo.description,
+    })
+  },
+  [todos]
+  )
+
   useEffect(() => {
     axios.get('http://localhost:3000/todo').then(({ data }) => {
       setTodos(data)
@@ -56,7 +93,26 @@ export const Top = () => {
       <h1 className={styles.heading}>ToDo一覧</h1>
       <ul className={styles.list}>
         {todos.map((todo) => {
-          return <ListItem key={todo.id} todo={todo} />
+          if (editTodoId === todo.id) {
+            return (
+              <li key={todo.id}>
+                <Form
+                  value={inputValues}
+                  editTodoId={editTodoId}
+                  onChange={handleInputChange}
+                  onCancelClick={handleCancelButtonClick}
+                  onSubmit={handleEditedTodoSubmit}
+                />
+              </li>
+            )
+          }
+          return (
+            <ListItem
+              key={todo.id}
+              todo={todo}
+              onEditButtonClick={handleEditButtonClick}
+            />
+          )
         })}
 
         <li>
