25ca853ef0d557ab95f58ed3362704bca165d3d3
keita mori <mk5.noki@gmail.com>
2023-11-06 10:18:44 +0900

    ADD: セクション21実装

---
+++ client/src/components/pages/completed/index.jsx
@@ -0,0 +1,11 @@
+import { Layout } from "../../ui/Layout"
+
+import styles from './index.module.css'
+
+export const Completed = () => {
+  return (
+    <Layout>
+      <h1 className={styles.heading}>完了済み</h1>
+    </Layout>
+  )
+}
---
+++ client/src/routes/index.jsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from 'react-router-dom'
+
+import { Top } from '../components/pages/Top'
+import { Completed } from '../components/pages/completed'
+
+export const router = createBrowserRouter([
+  { path: '/', element: <Top /> },
+  { path: '/completed', element: <Completed /> }
+])
