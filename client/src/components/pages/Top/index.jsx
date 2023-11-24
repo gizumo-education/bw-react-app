17c640b9e0e9f87e12f7fe2c41269f6485feb940
Yusaku0329 <nakamura.rizumu.nukesaku74@gmail.com>
2023-11-24 17:17:17 +0900

    add:ToDoの一覧表示追加

--- client/src/components/pages/Top/index.jsx
+++ client/src/components/pages/Top/index.jsx
@@ -1,11 +1,31 @@
 import { Layout } from '../../ui/Layout'
-
+import { ListItem } from '../../ui/ListItem'
+import { useState,useEffect } from 'react' // 追加
+import { axios } from '../../../utils/axiosConfig'
 import styles from './index.module.css'
 
 export const Top = () => {
+  const [todos, setTodos] = useState([]) // 追加
+  useEffect(() => {
+    axios.get('http://localhost:3000/todo').then(({ data }) => {
+      // API通信でTodoデータ引っ張り
+      console.log(data)
+      // todoデータ(配列*オブジェクト)をコンソールに表示
+      setTodos(data)
+    })
+  }, [])
+  
+  
+  
   return (
     <Layout>
       <h1 className={styles.heading}>ToDo一覧</h1>
+      <ul className={styles.list}>
+        {todos.map((todo) => {
+         
+          return <ListItem key={todo.id} todo={todo} />
+        })}
+      </ul>
     </Layout>
   )
 }
