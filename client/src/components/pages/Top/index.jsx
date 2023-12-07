bda7f51be6e54109c04fbb1a39a4ef6f22dfb942
taichi-watabe <124047993+taichi-watabe@users.noreply.github.com>
2023-12-07 17:31:01 +0900

    セクション17の練習問題の実装

--- client/src/components/pages/Top/index.jsx
+++ client/src/components/pages/Top/index.jsx
@@ -70,6 +70,11 @@ export const Top = () => {
       description: targetTodo.description,
     })
   }, [todos])
+  const handleDeleteButtonClick = useCallback((id) => {
+    axios.delete(`http://localhost:3000/todo/${id}`).then(({data}) => {
+      setTodos(data)
+    })
+  }, [])
   useEffect(() => {
     axios.get('http://localhost:3000/todo').then(({data}) => {
       // console.log(data);
@@ -100,6 +105,7 @@ export const Top = () => {
               key={todo.id}
               todo={todo}
               onEditButtonClick={handleEditButtonClick}
+              onDeleteButtonClick={handleDeleteButtonClick}
             />
           )
         })}
