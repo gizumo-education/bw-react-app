88fe1fd996180fcf3449e2ba2291367b77b93050
atsuikeda <ike.ke.no.ke@gmail.com>
2024-05-16 14:15:34 +0900

    React lesson section15 ToDoの追加 練習問題解答

--- client/src/components/pages/Top/index.jsx
+++ client/src/components/pages/Top/index.jsx
@@ -1,17 +1,53 @@
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 
-import { Layout } from '../../ui/Layout'
 import { axios } from '../../../utils/axiosConfig'
 
-import styles from './index.module.css'
+import { Button } from '../../ui/Button'
+import { Icon } from '../../ui/Icon'
+import { Layout } from '../../ui/Layout'
 import { ListItem } from '../../ui/ListItem'
+import { Form } from '../../ui/Form'
+
+import styles from './index.module.css'
 
 export const Top = () => {
   const [todos, setTodos] = useState([])
 
+  const [inputValues, setInputValues] = useState({
+    title: '',
+    description: '',
+  })
+  const [isAddTaskFormOpen, setIsAddTaskFormOpen] = useState(false)
+
+  const handleAddTaskButtonClick = useCallback(() => {
+    setIsAddTaskFormOpen(true)
+  }, [])
+
+  const handleCancelButtonClick = useCallback(() => {
+    setIsAddTaskFormOpen(false)
+  }, [])
+
+  const handleInputChange = useCallback((event) => {
+    const { name, value } = event.target
+    setInputValues((prev) => ({ ...prev, [name]: value }))
+  }, [])
+
+  const handleCreateTodoSubmit = useCallback(
+    (event) => {
+      event.preventDefault()
+      axios.post('http://localhost:3000/todo', inputValues).then(({ data }) => {
+        console.log(data)
+        setTodos((prevState) => ([...prevState, data]))
+        setIsAddTaskFormOpen(false)
+        setInputValues({ title: '', description: '' })
+      })
+    },
+    [inputValues]
+  )
+
   useEffect(() => {
     axios.get('http://localhost:3000/todo').then(({ data }) => {
-      setTodos(data);
+      setTodos(data)
     })
   }, [])
 
@@ -19,9 +55,33 @@ export const Top = () => {
     <Layout>
       <h1 className={styles.heading}>ToDo一覧</h1>
       <ul className='{styles.list'>
-        {todos.map((todo) => {
-          return <ListItem key={todo.id} todo={todo} />
-        })}
+        {todos.map((todo) => (
+          <ListItem key={todo.id} todo={todo} />
+        ))}
+        <li>
+          {isAddTaskFormOpen ? (
+            <Form
+              value={inputValues}
+              onChange={handleInputChange}
+              onCancelClick={handleCancelButtonClick}
+              onSubmit={handleCreateTodoSubmit}
+            />
+          ) : (
+            <Button
+              onClick={handleAddTaskButtonClick}
+              buttonStyle='indigo-blue'
+              className={styles['add-task']}
+            >
+              <Icon
+                iconName='plus'
+                color='orange'
+                size='medium'
+                className={styles['plus-icon']}
+              />
+              タスクを追加
+            </Button>
+          )}
+        </li>
       </ul>
     </Layout>
   )
