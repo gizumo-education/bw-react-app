5602cef887c2fb48107ba14d00031b6ed82a996e
atsuikeda <ike.ke.no.ke@gmail.com>
2024-05-17 18:41:11 +0900

    React Lesson section20 TodoアプリのSPA化　練習問題解答

---
+++ client/src/routes/index.jsx
@@ -0,0 +1,10 @@
+
+import { createBrowserRouter } from 'react-router-dom'
+
+import { Top } from '../components/pages/Top'
+import { Completed } from '../components/pages/Top/Completed'
+
+export const router = createBrowserRouter([
+  { path: '/', element: <Top /> },
+  { path: '/Completed', element: <Completed />}
+])
