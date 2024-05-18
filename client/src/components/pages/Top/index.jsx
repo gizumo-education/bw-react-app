614e9053cbb7088d9635a6c0d03fb5e96fc2d36c
akiho_matsymoto <a-matsumotogmail.com>
2024-05-18 17:40:43 +0900

    Todo編集と削除機能の追加

--- client/src/components/pages/Top/index.jsx
+++ client/src/components/pages/Top/index.jsx
@@ -1,11 +1,148 @@
+import { useState, useEffect, useCallback } from 'react'
+import { axios } from '../../../utils/axiosConfig'
+
 import { Layout } from '../../ui/Layout'
+import { ListItem } from '../../ui/ListItem'
+import { Button } from '../../ui/Button'
+import { Icon } from '../../ui/Icon'
+import { Form } from '../../ui/Form'
 
 import styles from './index.module.css'
 
 export const Top = () => {
+  const [todos, setTodos] = useState([])
+  const [editTodoId, setEditTodoId] = useState('')
+  const [inputValues, setInputValues] = useState({
+    title: '',
+    description: '',
+  })
+  const [isAddTaskFormOpen, setIsAddTaskFormOpen] = useState(false)
+
+  const handleAddTaskButtonClick = useCallback(() => {
+    setInputValues({title:'', description:''})
+    setEditTodoId('')
+    setIsAddTaskFormOpen(true)
+  }, [])
+
+  const handleCancelButtonClick = useCallback(() => {
+    setEditTodoId('')
+    setIsAddTaskFormOpen(false)
+  }, [])
+
+  const handleInputChange = useCallback((event) => {
+    const { name, value } = event.target
+    setInputValues((prev) => ({ ...prev, [name]: value }))
+  }, [])
+
+  const handleCreateTodoSubmit = useCallback(
+    (event) => {
+      event.preventDefault()
+      axios.post('http://localhost:3000/todo', inputValues).then(({ data }) => {
+        todos.push(data)
+        setTodos(todos)
+        setTodos((todos) => [...todos, data])
+        setIsAddTaskFormOpen(false)
+        setInputValues({title: '', description: ''})
+      })
+    }, [inputValues]
+  )
+
+  const handleEditedTodoSubmit = useCallback(
+    (event) => {
+      event.preventDefault()
+      axios
+        .patch(`http://localhost:3000/todo/${editTodoId}`, inputValues)
+        .then(({ data }) => {
+          setTodos(todos.map(
+            (todo) => todo.id === editTodoId ? data : todo
+          ))
+          setEditTodoId('')
+        })
+    },
+    [editTodoId, inputValues]
+  )
+
+  const handleEditButtonClick = useCallback(
+    (id) => {
+    setIsAddTaskFormOpen(false)
+    setEditTodoId(id)
+    const targetTodo = todos.find((todo) => todo.id === id)
+    setInputValues({
+      title: targetTodo.title,
+      description: targetTodo.description,
+    })
+  }, [todos])
+
+  const handleDeleteButtonClick = useCallback((id) => {
+    axios
+      .delete(`http://localhost:3000/todo/${id}`)
+      .then(({data}) => {
+        console.log(data);
+        setTodos(data)
+      })
+    console.log(id);
+  }, [])
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/todo').then(({ data }) => {
+      setTodos(data)
+    })
+  }, [])
+
   return (
     <Layout>
       <h1 className={styles.heading}>ToDo一覧</h1>
+
+      <ul className={styles.list}>
+        {todos.map((todo) => {
+          if (editTodoId === todo.id) {
+            return (
+              <li key={todo.id}>
+                <Form 
+                  value={inputValues}
+                  editTodoId={editTodoId}
+                  onChange={handleInputChange}
+                  onCancelClick={handleCancelButtonClick}
+                  onSubmit={handleEditedTodoSubmit}
+                />
+              </li>
+            )
+          }
+
+          return (
+            <ListItem 
+              key={todo.id}
+              todo={todo}
+              onEditButtonClick={handleEditButtonClick}
+              onDeleteButtonClick={handleDeleteButtonClick}
+            />
+          )
+        })}
+        <li>
+          {isAddTaskFormOpen ? (
+            <Form
+              value={inputValues}
+              onChange={handleInputChange}
+              onCancelClick={handleCancelButtonClick}
+              onSubmit={handleCreateTodoSubmit}
+            />
+          ) : (
+            <Button
+              buttonStyle='indigo-blue'
+              onClick={handleAddTaskButtonClick}
+              className={styles['add-task']}
+            >
+              <Icon
+                iconName='plus'
+                color='orange'
+                size='medium'
+                className={styles['plus-icon']}
+              />
+              インコ大発見
+            </Button>
+          )}
+        </li>
+      </ul>
     </Layout>
   )
 }
