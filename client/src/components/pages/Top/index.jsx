0b864f31170c69c57ede4e19f9f6b3f5db34777a
mihonakagawa <ohim212welldone@gmail.com>
2024-08-17 14:03:10 +0900

    feature: [sec14] get Todos and display the Todo list

--- client/src/components/pages/Top/index.jsx
+++ client/src/components/pages/Top/index.jsx
@@ -1,11 +1,30 @@
+import { useState, useEffect } from 'react'
+import { axios } from '../../../utils/axiosConfig'
 import { Layout } from '../../ui/Layout'
+import { ListItem } from '../../ui/ListItem'
+
 
 import styles from './index.module.css'
 
 export const Top = () => {
+  const [todos, setTodos] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/todo').then(({ data }) => {
+      // console.log('data:', data) // Todo情報3つ分
+      setTodos(data)
+    })
+  }, [])
+
   return (
     <Layout>
       <h1 className={styles.heading}>ToDo一覧</h1>
+      <ul className={styles.list}>
+        {todos.map((todo) => {
+          // console.log('todo:', todo) // Todo情報1つずつ
+          return <ListItem key={todo.id} todo={todo} />
+        })}
+      </ul>
     </Layout>
   )
 }
