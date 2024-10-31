4c4d1309ab0f7a4953a85a7107dc14286f9112a2
kiyosakitaisei <kiyotai06@gmail.com>
2024-10-31 10:59:35 +0900

    docs:Section18,19練習問題

--- client/src/components/pages/Top/index.jsx
+++ client/src/components/pages/Top/index.jsx
@@ -5,6 +5,7 @@ import { ListItem } from '../../ui/ListItem'
 import { Button } from '../../ui/Button'
 import { Icon } from '../../ui/Icon'
 import { Form } from '../../ui/Form'
+import { errorToast } from '../../../utils/errorToast'
 
 import styles from './index.module.css'
 
@@ -13,135 +14,208 @@ export const Top = () => {
   // editTodoId:編集ボタンがクリックされた時に編集するToDoのidを格納
   const [editTodoId, setEditTodoId] = useState('')
 
-    // ↓ inputValuesというstateを追加 ↓
-    const [inputValues, setInputValues] = useState({
-      title: '',  // タスク名
-      description: '',  // タスクの説明
-    })
+  // ↓ inputValuesというstateを追加 ↓
+  const [inputValues, setInputValues] = useState({
+    title: '',  // タスク名
+    description: '',  // タスクの説明
+  })
 
-    const [isAddTaskFormOpen, setIsAddTaskFormOpen] = useState(false) // 追加フォームの表示・非表示を管理するためのstate
+  const [isAddTaskFormOpen, setIsAddTaskFormOpen] = useState(false) // 追加フォームの表示・非表示を管理するためのstate
 
-    // タスクを追加」ボタンをクリックした時に実行するhandleAddTaskButtonClick関数
-    const handleAddTaskButtonClick = useCallback(() => {
-      // ToDoの追加フォームを開いた時にinputValuesに空文字を格納する
-      setInputValues({ title: '', description: '' })
-      // ToDoの追加フォームを表示するボタンをクリックした時に、editTodoIdに空文字を格納し、ToDoの編集フォームを非表示にする
-      setEditTodoId('')
-      setIsAddTaskFormOpen(true)
-    }, [])
+  // タスクを追加」ボタンをクリックした時に実行するhandleAddTaskButtonClick関数
+  const handleAddTaskButtonClick = useCallback(() => {
+    // ToDoの追加フォームを開いた時にinputValuesに空文字を格納する
+    setInputValues({ title: '', description: '' })
+    // ToDoの追加フォームを表示するボタンをクリックした時に、editTodoIdに空文字を格納し、ToDoの編集フォームを非表示にする
+    setEditTodoId('')
+    setIsAddTaskFormOpen(true)
+  }, [])
 
-    // ↓ キャンセルボタンをクリックした時にisAddTaskFormOpenの値をfalseに更新
-    const handleCancelButtonClick = useCallback(() => {
-      // 編集フォームの「キャンセル」ボタンをクリックした時に、editTodoIdに空文字を格納することで、先ほど追加したif文の条件に一致しなくなり、編集フォームが非表示になる
-      setEditTodoId('')
-      setIsAddTaskFormOpen(false)
-    }, [])
+  // ↓ キャンセルボタンをクリックした時にisAddTaskFormOpenの値をfalseに更新
+  const handleCancelButtonClick = useCallback(() => {
+    // 編集フォームの「キャンセル」ボタンをクリックした時に、editTodoIdに空文字を格納することで、先ほど追加したif文の条件に一致しなくなり、編集フォームが非表示になる
+    setEditTodoId('')
+    setIsAddTaskFormOpen(false)
+  }, [])
 
-    // handleInputChange関数は、引数にeventを受け取り、event.targetからnameとvalueを取得しています。
-    // event.targetからnameとvalueを取得して、setInputValues関数を使ってinputValuesの値を更新しています。
-    const handleInputChange = useCallback((event) => {
-      const { name, value } = event.target // event.target - イベントが発生した要素を取得できるオブジェクト
-      setInputValues((prev) => ({ ...prev, [name]: value }))
-    }, [])
+  // handleInputChange関数は、引数にeventを受け取り、event.targetからnameとvalueを取得しています。
+  // event.targetからnameとvalueを取得して、setInputValues関数を使ってinputValuesの値を更新しています。
+  const handleInputChange = useCallback((event) => {
+    const { name, value } = event.target // event.target - イベントが発生した要素を取得できるオブジェクト
+    setInputValues((prev) => ({ ...prev, [name]: value }))
+  }, [])
 
-    // 追加ボタン
-    // 引数にeventを受け取り、event.preventDefault()でformタグのデフォルトのイベントであるページ遷移をキャンセルしている
-    // axiosを使用してAPI通信を行いToDoを追加。axiosのpostメソッドの第一引数にはAPIのURLを指定し、第二引数には送信するデータを指定。
-    const handleCreateTodoSubmit = useCallback(
-      (event) => {
-        event.preventDefault()
-        axios.post('http://localhost:3000/todo', inputValues).then(({ data }) => {
-          // console.log(data)
-
-          // 問:API通信が成功した場合、追加したToDoが一覧に表示されるようにしてください
-          // todosは配列、dataは配列でない。だからtodosを展開してdataを配列に入れる。
-          setTodos([...todos,data])
-
-          // 問:API通信が成功した場合、ToDoの追加フォームを非表示にしてください
-          setIsAddTaskFormOpen(false)
-
-          // 問:API通信が成功した場合、ToDoの追加フォームの入力欄を空にしてください
-          setInputValues({
-            title: '',
-            description: '',
-          });
-        })
-      },
-      [inputValues]
-    )
-
-    // 編集保存ボタン
-    // handleEditedTodoSubmit関数は引数にeventを受け取り、event.preventDefault()でformタグのデフォルトのイベントであるページ遷移をキャンセルしています。
-    // その後、axiosを使用してAPI通信を行い、ToDoのタイトルと説明を更新しています。
-    // axiosのpatchメソッドの第一引数にはAPIのURLを指定し、第二引数には送信するデータを指定します。
-    // API通信が成功した場合は、APIからのレスポンスをコンソールに出力しています。
-    const handleEditedTodoSubmit = useCallback(
-      (event) => {
-        event.preventDefault()
-        axios
-          .patch(`http://localhost:3000/todo/${editTodoId}`, inputValues)
-          .then(({ data }) => {
-            // console.log(data)
-            // console.log(todos)
-
-            // 問:ToDoの編集が成功した場合、編集したToDoのタイトルと説明をToDoの一覧に反映させてください
-            // todosには編集前一覧の配列が入っている。
-            // valにはdataには
-            setTodos(todos.map(val =>
-              val.id === data.id ? data : val
-            ))
-            // console.log(setTodos)
-
-            // 繰り返し処理したい配列.map(コールバック関数);
-            // 条件式 ? trueの時の処理 : falseの時の処理
-            // 全体.古いID === 更新.新ID ? 更新ID : 古いID
-            // console.log(val)
-            
-            // 問:ToDoの編集が成功した場合、ToDoの編集フォームを非表示にしてください
-            setEditTodoId('')
-          })
-      },
-      [editTodoId, inputValues]
-    )
-
-    // ↓ editTodoIdに編集するToDoのidを格納
-    // ToDoの編集フォームを表示するボタン
-    const handleEditButtonClick = useCallback(
-      (id) => {
-        // ToDoの編集フォームを表示するボタンをクリックした時に、isAddTaskFormOpenをfalseに変更し、ToDoの追加フォームを非表示にする
-        setIsAddTaskFormOpen(false)
-        setEditTodoId(id)
-        // ↓ 編集ボタンがクリックされた時に、todosに格納されたToDoの中から、編集するToDoのidと一致するToDoをArrayメソッドのfind()を使用して取得し、inputValuesに格納しています。
-        // これで、ToDoの編集フォームを開いた時に、フォームに編集するToDoのタイトルと説明が表示されるようになりました。
-        const targetTodo = todos.find((todo) => todo.id === id)
-        setInputValues({
-          title: targetTodo.title,
-          description: targetTodo.description,
-        })
-      },
-      [todos] // 依存配列にtodosを追加。つまり、、？
-    )
-
-    // 削除ボタン
-    // handleDeleteButtonClick関数は、引数に削除するToDoのidを受け取ります。
-    // 問:handleDeleteButtonClick関数の中身の処理を書いてToDoの削除機能を完成させてください。
-    const handleDeleteButtonClick = useCallback((id) => {
-      axios.delete(`http://localhost:3000/todo/${id}`, inputValues).then(({ data }) => {
-        // console.log(setTodos)
+  // 追加ボタン
+  // 引数にeventを受け取り、event.preventDefault()でformタグのデフォルトのイベントであるページ遷移をキャンセルしている
+  // axiosを使用してAPI通信を行いToDoを追加。axiosのpostメソッドの第一引数にはAPIのURLを指定し、第二引数には送信するデータを指定。
+  const handleCreateTodoSubmit = useCallback(
+    (event) => {
+      event.preventDefault()
+      axios.post('http://localhost:3000/todo', inputValues).then(({ data }) => {
         // console.log(data)
-        // console.log(todos)
-        setTodos(data)
-        console.log(data)
+
+        // 問:API通信が成功した場合、追加したToDoが一覧に表示されるようにしてください
+        // todosは配列、dataは配列でない。だからtodosを展開してdataを配列に入れる。
+        setTodos([...todos, data])
+
+        // 問:API通信が成功した場合、ToDoの追加フォームを非表示にしてください
+        setIsAddTaskFormOpen(false)
+
+        // 問:API通信が成功した場合、ToDoの追加フォームの入力欄を空にしてください
+        setInputValues({
+          title: '',
+          description: '',
+        });
+      })
+        // ネットワークエラー
+        .catch((error) => {
+          errorToast(error.message)
+        })
+    },
+    [inputValues]
+  )
+
+  // 編集保存ボタン
+  // handleEditedTodoSubmit関数は引数にeventを受け取り、event.preventDefault()でformタグのデフォルトのイベントであるページ遷移をキャンセルしています。
+  // その後、axiosを使用してAPI通信を行い、ToDoのタイトルと説明を更新しています。
+  // axiosのpatchメソッドの第一引数にはAPIのURLを指定し、第二引数には送信するデータを指定します。
+  // API通信が成功した場合は、APIからのレスポンスをコンソールに出力しています。
+  const handleEditedTodoSubmit = useCallback(
+    (event) => {
+      event.preventDefault()
+      axios
+        .patch(`http://localhost:3000/todo/${editTodoId}`, inputValues)
+        .then(({ data }) => {
+          // console.log(data)
+          // console.log(todos)
+
+          // 問:ToDoの編集が成功した場合、編集したToDoのタイトルと説明をToDoの一覧に反映させてください
+          // todosには編集前一覧の配列が入っている。
+          // valにはdataには
+          setTodos(todos.map(val =>
+            val.id === data.id ? data : val
+          ))
+          // console.log(setTodos)
+
+          // 繰り返し処理したい配列.map(コールバック関数);
+          // 条件式 ? trueの時の処理 : falseの時の処理
+          // 全体.古いID === 更新.新ID ? 更新ID : 古いID
+          // console.log(val)
+
+          // 問:ToDoの編集が成功した場合、ToDoの編集フォームを非表示にしてください
+          setEditTodoId('')
+        })
+        // ToDoの編集のエラーハンドリング
+        .catch((error) => {
+          switch (error.statusCode) {
+            case 404:
+              errorToast(
+                '更新するToDoが見つかりませんでした。画面を更新して再度お試しください。'
+              )
+              break
+            // ネットワークエラー
+            default:
+              errorToast(error.message)
+              break
+          }
+        })
+    },
+    [editTodoId, inputValues]
+  )
+
+  // ↓ editTodoIdに編集するToDoのidを格納
+  // ToDoの編集フォームを表示するボタン
+  const handleEditButtonClick = useCallback(
+    (id) => {
+      // ToDoの編集フォームを表示するボタンをクリックした時に、isAddTaskFormOpenをfalseに変更し、ToDoの追加フォームを非表示にする
+      setIsAddTaskFormOpen(false)
+      setEditTodoId(id)
+      // ↓ 編集ボタンがクリックされた時に、todosに格納されたToDoの中から、編集するToDoのidと一致するToDoをArrayメソッドのfind()を使用して取得し、inputValuesに格納しています。
+      // これで、ToDoの編集フォームを開いた時に、フォームに編集するToDoのタイトルと説明が表示されるようになりました。
+      const targetTodo = todos.find((todo) => todo.id === id)
+      setInputValues({
+        title: targetTodo.title,
+        description: targetTodo.description,
       })
     },
-    []
+    [todos] // 依存配列にtodosを追加。つまり、、？
   )
-    useEffect(() => {
-      axios.get('http://localhost:3000/todo').then(({ data }) => {
-        // console.log(data)
-        setTodos(data)
+
+  // 削除ボタン
+  // handleDeleteButtonClick関数は、引数に削除するToDoのidを受け取ります。
+  // 問:handleDeleteButtonClick関数の中身の処理を書いてToDoの削除機能を完成させてください。
+  const handleDeleteButtonClick = useCallback((id) => {
+    axios.delete(`http://localhost:3000/todo/${id}`, inputValues).then(({ data }) => {
+      // console.log(setTodos)
+      // console.log(data)
+      // console.log(todos)
+      setTodos(data)
+      console.log(data)
+    })
+      // ToDoの削除のエラーハンドリング
+      .catch((error) => {
+        switch (error.statusCode) {
+          case 404:
+            errorToast(
+              '削除するToDoが見つかりませんでした。画面を更新して再度お試しください。'
+            )
+            break
+          // ネットワークエラー
+          default:
+            errorToast(error.message)
+            break
+        }
       })
-    }, [])
+  }, [])
+
+  // ↓ ToDoの完了・未完了切り替え機能の実装
+  // handleToggleButtonClick関数は、引数に完了・未完了の切り替え対象のToDoのidを受け取ります。
+  // axiosのpatchメソッドの第一引数にはAPIのURLを指定し、第二引数には完了・未完了を切り替えるToDoのisCompletedの値を指定します。
+  // API通信が成功した場合は、APIからのレスポンスをコンソールに出力しています。
+  const handleToggleButtonClick = useCallback(
+    (id) => {
+      axios
+        .patch(`http://localhost:3000/todo/${id}/completion-status`, {
+          isCompleted: todos.find((todo) => todo.id === id).isCompleted,
+        })
+        .then(({ data }) => {
+          // ToDoの完了・未完了の切り替えが成功した場合、切り替え後のToDoの完了・未完了の状態を画面に反映させてください。
+
+          console.log(data)// 1
+          console.log(todos)// 3 val
+
+          setTodos(todos.map(todo =>
+            todo.id === data.id ? data : todo
+          ))
+        })
+        // ToDoの完了・未完了の切り替えのエラーハンドリング
+        .catch((error) => {
+          switch (error.statusCode) {
+            case 404:
+              errorToast(
+                '完了・未完了を切り替えるToDoが見つかりませんでした。画面を更新して再度お試しください。'
+              )
+              break
+            // ネットワークエラー
+            default:
+              errorToast(error.message)
+              break
+          }
+        })
+    },
+    [todos]
+  )
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/todo').then(({ data }) => {
+      // console.log(data)
+      setTodos(data)
+    })
+      // ネットワークエラー
+      .catch((error) => {
+        errorToast(error.message)
+      })
+  }, [])
 
   return (
     <Layout>
@@ -162,13 +236,14 @@ export const Top = () => {
               </li>
             )
           }
-          return(
+          return (
             <ListItem
               key={todo.id}
               todo={todo}
               // 編集ボタンがクリックされた時に、handleEditButtonClick関数が実行され、editTodoIdに編集するToDoのidを格納できるようになりました。
               onEditButtonClick={handleEditButtonClick}
               onDeleteButtonClick={handleDeleteButtonClick} // 削除
+              onToggleButtonClick={handleToggleButtonClick} // ToDoの状態変更
             />
           )
         })}
@@ -186,11 +261,11 @@ export const Top = () => {
               onSubmit={handleCreateTodoSubmit} // 
             />
           ) : (
-              <Button
-                buttonStyle='indigo-blue'
-                onClick={handleAddTaskButtonClick} // 「タスクを追加」ボタンをクリックした時にisAddTaskFormOpenの値をtrueに更新する処理
-                className={styles['add-task']}
-              >
+            <Button
+              buttonStyle='indigo-blue'
+              onClick={handleAddTaskButtonClick} // 「タスクを追加」ボタンをクリックした時にisAddTaskFormOpenの値をtrueに更新する処理
+              className={styles['add-task']}
+            >
               <Icon
                 iconName='plus'
                 color='orange'
--- client/src/components/ui/ListItem/index.jsx
+++ client/src/components/ui/ListItem/index.jsx
@@ -8,36 +8,73 @@ import { Icon } from '../Icon'
 import styles from './index.module.css'
 
 export const ListItem = memo(
-  ({ todo, onEditButtonClick, onDeleteButtonClick }) => {
-  return (
-    <li className={styles['list-item']}>
-      <div className={styles.task}>
-        <div className={styles.title}>{todo.title}</div>
-        {todo.description && (
-          <div className={styles.description}>{todo.description}</div>
+  ({ todo, onEditButtonClick, onDeleteButtonClick, onToggleButtonClick }) => {
+    return (
+      <li className={styles['list-item']}>
+        {/* // ToDoの状態変更（完了・未完了の切り替え） */}
+        {todo.isCompleted ? (
+          <Button
+            buttonStyle='icon-only'
+            className={styles['complete-button']}
+            onClick={() => onToggleButtonClick(todo.id)}
+          >
+            <Icon iconName='check' size='large' color='orange' />
+          </Button>
+        ) : (
+          <Button
+            buttonStyle='icon-only'
+            className={styles['complete-button']}
+            onClick={() => onToggleButtonClick(todo.id)}
+          >
+            <Icon
+              iconName='circle'
+              size='medium'
+              className={styles['circle-icon']}
+            />
+          </Button>
         )}
-      </div>
 
-    {/* 編集ボタンが表示されている */}
-    {/* 編集ボタンをクリックした時に、onEditButtonClickというpropsで渡された関数を実行する */}
-    <div className={styles['task-action']}>
-      <Button
-        buttonStyle='icon-only'
-        onClick={() => onEditButtonClick(todo.id)}
-      >
-        <Icon iconName='edit' color='indigo-blue' size='medium' />
-      </Button>
-      {/* 削除ボタン */}
-        <Button
-          buttonStyle='icon-only'
-          onClick={() => onDeleteButtonClick(todo.id)}
-        >
-          <Icon iconName='trash' color='indigo-blue' size='medium' />
-        </Button>
-    </div>
-    </li>
-  )
-})
+        <div className={styles.task}>
+          <div
+            // ↓ classNameに'task-completed'を追加
+            // todo.isCompletedがtrueの場合は、task-completedというクラスを付与するようにしています。
+            // task-completedクラスには、打ち消し線を引くためのスタイルが定義されています。
+            className={`${styles.title} ${todo.isCompleted ? styles['task-completed'] : ''
+              }`}
+          >
+            {todo.title}
+          </div>
+          {todo.description && (
+            <div
+              // ↓ classNameに'task-completed'を追加
+              className={`${styles.description} ${todo.isCompleted ? styles['task-completed'] : ''
+                }`}
+            >
+              {todo.description}
+            </div>
+          )}
+        </div>
+
+        {/* 編集ボタンが表示されている */}
+        {/* 編集ボタンをクリックした時に、onEditButtonClickというpropsで渡された関数を実行する */}
+        <div className={styles['task-action']}>
+          <Button
+            buttonStyle='icon-only'
+            onClick={() => onEditButtonClick(todo.id)}
+          >
+            <Icon iconName='edit' color='indigo-blue' size='medium' />
+          </Button>
+          {/* 削除ボタン */}
+          <Button
+            buttonStyle='icon-only'
+            onClick={() => onDeleteButtonClick(todo.id)}
+          >
+            <Icon iconName='trash' color='indigo-blue' size='medium' />
+          </Button>
+        </div>
+      </li>
+    )
+  })
 
 ListItem.displayName = 'ListItem'
 ListItem.propTypes = {
@@ -49,4 +86,5 @@ ListItem.propTypes = {
   }).isRequired,
   onEditButtonClick: PropTypes.func.isRequired, // 編集
   onDeleteButtonClick: PropTypes.func.isRequired, // 削除
+  onToggleButtonClick: PropTypes.func.isRequired, // 追加
 }
